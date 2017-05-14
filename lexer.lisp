cf77b27a4f3c3341d180da92fdd97f279c76eff4
Ashurnasirpal <poponnno1@gmail.com>
2017-05-15 02:27:33 +0900

    lexer-stream 実装

--- lexer.lisp
+++ lexer.lisp
@@ -2,18 +2,18 @@
 
 (define-string-lexer closh-lexer
   ("\\s")
-  ("\\(" (return (values :lparen $@)))
-  ("'\\(" (return (values :quote-lparen $@)))
-  ("\\)" (return (values :rparen $@)))
-  ("\\." (return (values :period $@)))
-  ("#f" (return (values :false $@)))
-  ("#t" (return (values :true $@)))
+  ("\\(" (return (values :lparen :lparen)))
+  ("'\\(" (return (values :quote-lparen :quote-lparen)))
+  ("\\)" (return (values :rparen :rparen)))
+  ("\\." (return (values :period :period)))
+  ("#f" (return (values :bool :false)))
+  ("#t" (return (values :bool :true)))
   ("[0-9A-Za-z!$%&*+-./<=>?@^_]+"
    (if (string= (scan-to-strings
                  "-?(0|[1-9][0-9]*(\\.[0-9]*)?)" $@) $@)
-       (return (values :num $@))
-       (return (values :sym $@))))
-  ("." (return (values :undefined $@))))
+       (return (values :num (read-from-string $@)))
+       (return (values :sym (intern $@ :keyword)))))
+  ("." (return (values :undefined :undefined))))
 
 (defun lexer-test (str)
   (funcall (alambda (lex)
--- parser.lisp
+++ parser.lisp
@@ -1,4 +1,19 @@
 (in-package :closh)
 
-(defun closh-parser (lexer)
-  (declare (ignore lexer)))
+(define-class lexer-stream ()
+  lexer (hold nil))
+
+(defgeneric look-ahead (stream))
+(defmethod look-ahead ((stream lexer-stream))
+  (when (null (hold stream))
+    (multiple-value-bind (tag val) (funcall (lexer stream))
+      (setf (hold stream) (cons tag val))))
+  (values (car (hold stream)) (cdr (hold stream))))
+
+(defgeneric consume-token (stream))
+(defmethod consume-token ((stream lexer-stream))
+  (aif (hold stream)
+       (progn
+         (setf (hold stream) nil)
+         (values (car it) (cdr it)))
+       (funcall (lexer stream))))
